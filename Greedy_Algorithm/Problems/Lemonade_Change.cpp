95180a30c3fb84191ce824d5f87ecca1857da8b0
satya prakash <satya200prakash@gmail.com>
2025-10-27 18:07:19 +0530

    QN)860. Lemonade Change

---
+++ Greedy_Algorithm/Problems/Lemonade_Change.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0, ten = 0;
+        for(int i=0;i<bills.size();i++){
+            if(bills[i] == 5) five++;
+            else if(bills[i] == 10){
+                if(five){
+                    five -= 1;
+                    ten += 1;
+                }
+                else return false;
+            }
+            else{
+                if(five && ten){
+                    five -= 1;
+                    ten -= 1;
+                }
+                else if(five >= 3){
+                    five -= 3;
+                }
+                else return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+int main(){
+Solution s;
+vector<int> bills = {5, 5, 10, 10, 20};
+cout<<"isPossible make: "<<s.lemonadeChange(bills);
+
+    return 0;
+}
